04ce398ecace5dcc578796c73d5f179827f683fe
jeevanthask <jeevanthanandika@gmail.com>
2023-06-17 19:49:06 +0530

    material ui added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h2>App</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        totam sit et ipsa assumenda saepe aliquid vitae nulla veniam ullam,
+        dolores animi nostrum hic suscipit quae. Eos mollitia, deleniti sed quod
+        quo architecto animi voluptates, modi ipsa quam molestiae, facere
+        deserunt officiis fuga soluta delectus nisi maiores illum? Nesciunt, ab!
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
