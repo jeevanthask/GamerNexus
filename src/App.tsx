b60690337f182c75ad411aafac9a7cbbb0d1b9c5
jeevanthask <jeevanthanandika@gmail.com>
2023-06-22 21:42:50 +0530

    react bootstrap navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import NavBar from "./components/Navbar";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <NavBar />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={3} style={{ backgroundColor: "pink" }}>
+          2 of 3 (wider)
+        </Col>
+        <Col xs={9} style={{ backgroundColor: "orange" }}>
+          3 of 3
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
