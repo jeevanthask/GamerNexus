9348aef21ae84090d626d861c81b8a07357c928d
jeevanthask <jeevanthanandika@gmail.com>
2023-06-22 21:24:54 +0530

    mui remooved

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,7 @@
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import NavBar from "./components/NavBar";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+import React from "react";
 
 function App() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={0}>
-        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-          <NavBar />
-        </Grid>
-        <Grid
-          item
-          md={4}
-          lg={4}
-          xl={4}
-          sx={{ display: { xs: "none", md: "block" } }}
-        >
-          <Item style={{ color: "black", backgroundColor: "#00e676" }}>
-            Aside
-          </Item>
-        </Grid>
-        <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
-          <Item style={{ color: "black", backgroundColor: "##ffea00" }}>
-            Main Content goes here
-          </Item>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,33 +1,7 @@
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import Box from "@mui/material/Box";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
+import React from "react";
 
 function NavBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+  return <div>NavBar</div>;
 }
 
 export default NavBar;
